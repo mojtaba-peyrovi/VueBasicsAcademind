b65fc060aa57f6c27ea0cbb2188a78f53f06f5db
= <mojtaba.peyrovi@gmail.com>
2018-02-18 07:43:30 +0700

    end of part-10:what I learned: part9:making different components and making them interact to each other. part10:props and events explained.

---
+++ 08/vue-webpack/src/Message.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="">
+    <h1>{{ message }}</h1>
+    <app-input :msg="message" @messageChange="message = $event"></app-input>
+  </div>
+</template>
+<script>
+import Input from './Input.vue';
+
+export default {
+  data(){
+    return {
+      message: 'This is a great message'
+    }
+  },
+  components: {
+    'app-input': Input
+  }
+}
+</script>
--- 08/vue-webpack/src/main.js
+++ 08/vue-webpack/src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Message from './Message.vue';
+
+Vue.component('app-message',Message);
+
+
+
 
 new Vue({
   el: '#app',
